3c0dfb1a0662653ff3c16bc16886d3d9c47a8fa1
moonhojun <treehj007@gmail.com>
2022-08-08 17:14:11 +0900

    update

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 # -*- coding:utf-8 -*-
-from flask import Flask
+from flask import Flask,
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello World!"
+@app.route('/api/calCulator', methods=['POST'])
+def calCulator():
+    body = request.get_json()
+    print(body)
+    params_df = body['action']['params']
+    print(type(params_df))
+    opt_operator = params_df['operators']
+    number01 = json.loads(params_df['sys_number01'])['amount']
+    number02 = json.loads(params_df['sys_number02'])['amount']
+
+    print(opt_operator, type(opt_operator), number01, type(number01))
+
+    answer_text = str(cals(opt_operator, number01, number02))
+
+    responseBody = {
+        "version": "2.0",
+        "template": {
+            "outputs": [
+                {
+                    "simpleText": {
+                        "text": answer_text
+                    }
+                }
+            ]
+        }
+    }
+
+    return responseBody
+
+if __name__ == "__main__":
+    app.run(port=5000)
