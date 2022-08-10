2827ea4242b2039585a7a85b10da8609f3c6e0b2
moonhojun <treehj007@gmail.com>
2022-08-10 16:55:47 +0900

    update

--- app.py
+++ app.py
@@ -1,22 +1,33 @@
 # -*- coding:utf-8 -*-
-from flask import Flask,
+from flask import Flask, request
 import json
-
+import pymysql
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
 app = Flask(__name__)
 
-@app.route('/api/calCulator', methods=['POST'])
-def calCulator():
-    body = request.get_json()
+def makequestion:
+    connect = pymysql.connect(host='localhost', user='root', password='패스워드', db='DB명',\
+                          charset='utf8mb4')
+    cur = connect.cursor()
+
+    query = 
+    cur.excute(query)
+    connect.commit()
+    #데이터 베이스 두번째에 있는 설명부분을 가지고 온다
+    question = #데이터 베이스에서 가지고 오기
+    answerlist = #보기 1 , 보기 2, 보기 3
+    answer = #데이터 베이스 앞에꺼
+
+    return 
+
+def db_create():
+    engine = create_engine("postgres://"uxweficayqkvnb:191795f6687a563f2d49dd25fa1d4a3b481604b2bfb416f11811f430377a463f@ec2-54-225-234-165.compute-1.amazonaws.com:5432/ddtk33j69v200c", echo = False)
+
+@app.route('/api/sayHello', methods=['POST'])
+def sayHello():
+    body = request.get_json() # 사용자가 입력한 데이터
     print(body)
-    params_df = body['action']['params']
-    print(type(params_df))
-    opt_operator = params_df['operators']
-    number01 = json.loads(params_df['sys_number01'])['amount']
-    number02 = json.loads(params_df['sys_number02'])['amount']
-
-    print(opt_operator, type(opt_operator), number01, type(number01))
-
-    answer_text = str(cals(opt_operator, number01, number02))
+    print(body['userRequest']['utterance'])
 
     responseBody = {
         "version": "2.0",
@@ -24,7 +35,7 @@ def calCulator():
             "outputs": [
                 {
                     "simpleText": {
-                        "text": answer_text
+                        "text": "안녕 hello I'm Ryan"
                     }
                 }
             ]
